fa347b6f4f89f85dca09ce20d95058ab806187e4
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-03 17:41:25 +0000

    Deleting files

--- chapter1/quiz1/questions/question_1.rb
+++
@@ -1,5 +0,0 @@
-# Write a program that `puts`es an integer e.g. `160`.
-
-puts 160
-
-
--- chapter1/quiz1/spec/question_1_spec.rb
+++
@@ -1,6 +0,0 @@
-describe "Q1. Height" do
-  it "`puts`es the height of the learner as a number" do
-    expect { require_relative "../questions/question_1" }
-      .to output(/\d+/).to_stdout
-  end
-end
