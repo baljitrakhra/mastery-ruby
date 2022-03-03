e775513007166bf05997ba939f38dbf65f9a974c
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-03 17:24:32 +0000

    Adding chapter 1 to chapter 2

---
+++ chapter4/quiz1/questions/question_2.rb
@@ -0,0 +1,11 @@
+# Write a program that adds together all the integers from `1` to
+# `250` (inclusive) and `puts`es the total.
+
+number = 0
+count = 0
+while count < 251 do
+  number = number + count
+  count = count + 1
+end
+puts number
+  
