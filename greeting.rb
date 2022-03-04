71a6f8d6a63f2c43871a6a22f5baaba24038cfa9
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-04 10:40:47 +0000

    Refactored Greeting program

--- greeting.rb
+++ greeting.rb
@@ -1,7 +1,4 @@
 puts "Please enter your first name :"
 name = gets.chomp.capitalize
-if name.chr == "S"
-    puts name.upcase
-else
-    puts "Hi,#{name}"
-end
+name.chr == "S" ? (puts name.upcase) : (puts "Hi,#{name}")
+
